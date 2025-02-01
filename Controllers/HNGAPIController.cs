ad333ca94ea1f17d23733394e45f9c209fa104ce
Habeeb958 <akoredehabeeb958@gmail.com>
2025-02-01 11:56:03 +0100

    initialcreate

---
+++ Controllers/HNGAPIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNGAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HNGAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var response = new
+            {
+                RegisteredEmail = "akoredehabeeb958@gmail.com",
+                CurrentDateTime = DateTime.UtcNow.ToString("o"),
+                GitHubURL = "https://github.com/Habeeb958/HNG12PublicAPI.git" // Replace with your GitHub repo URL
+            };
+
+            return Ok(response);
+        }
+    
+    }
+}
